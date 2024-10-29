d241eb364bacc1dd9b43d031cb7edc9a2b2d5f36
Your Name <you@example.com>
2024-10-29 15:11:14 +0530

    Login Ui done

---
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -0,0 +1,26 @@
+package lk.ijse.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+
+    @FXML
+    private AnchorPane Anchor;
+
+    @FXML
+    private AnchorPane ChildPane;
+
+    @FXML
+    private TextField UserName;
+
+    @FXML
+    private ComboBox<?> cmbAdmin;
+
+    @FXML
+    private PasswordField passward;
+
+}
